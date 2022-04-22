e4e0de5491cf228632f18b9fa60b1f087c6b2c0f
Andrei Budevich <budevichac01@gmail.com>
2022-04-22 16:23:52 +0300

    refactored DishTestData

--- src/test/java/by/restaurantvoting/testdata/DishTestData.java
+++ src/test/java/by/restaurantvoting/testdata/DishTestData.java
@@ -2,6 +2,8 @@ package by.restaurantvoting.testdata;
 
 import by.restaurantvoting.model.Dish;
 
+import java.util.Set;
+
 import static by.restaurantvoting.testdata.MenuTestData.*;
 import static by.restaurantvoting.testdata.RestaurantTestData.*;
 
@@ -24,47 +26,63 @@ public class DishTestData {
 
     public static final Dish dish0 = new Dish(DISH_ID_0, "Мачанка с драниками",
             "драники, куриное филе, ветчина, морковь, лук, шампиньоны, сливочный соус с укропом",
-            290, 67, restaurant0, restaurant0Menu0, restaurant0Menu1);
+            290, 67, restaurant0);
     public static final Dish dish1 = new Dish(DISH_ID_1, "Салат с курицей и помидорами",
             "листья салата, морковь, куриное филе, помидоры, огурцы, заправка для салата",
-            150, 32, restaurant0, restaurant0Menu0);
+            150, 32, restaurant0);
     public static final Dish dish2 = new Dish(DISH_ID_2, "Куриный суп с рисом",
             "картофель, куриное филе, лук, морковь, рис, зелень",
-            250, 31, restaurant0, restaurant0Menu1);
+            250, 31, restaurant0);
     public static final Dish dish3 = new Dish(DISH_ID_3, "Салат с ветчиной и кукурузой",
             "листья салата, морковь, огурцы, ветчина, консервированная кукуруза, подаются с заправкой для салата",
-            2145, 34, restaurant0, restaurant0Menu1);
+            2145, 34, restaurant0);
     public static final Dish dish4 = new Dish(DISH_ID_4, "Биточки с грибным соусом",
             "биточки из свинины, подаются со сливочным соусом с шампиньонами, помидорами и зеленью, картофельное пюре",
-            335, 75, restaurant0, restaurant0Menu1);
+            335, 75, restaurant0);
 
     public static final Dish dish5 = new Dish(DISH_ID_5, "Пицца Народная",
             "соус из протертых томатов, Моцарелла, ветчина, шампиньоны, маринованные огурцы, приправа к пицце, масло чесночное",
-            290, 67, restaurant1, restaurant1Menu0, restaurant1Menu1);
+            290, 67, restaurant1);
     public static final Dish dish6 = new Dish(DISH_ID_6, "Пицца Пикантная",
             "соус из протертых томатов, Моцарелла, курица, ананасы, приправа к пицце, масло чесночное",
-            290, 67, restaurant1, restaurant1Menu0);
+            290, 67, restaurant1);
     public static final Dish dish7 = new Dish(DISH_ID_7, "Пицца Повседневная",
             "соус из протертых томатов, Моцарелла, салями, помидоры, приправа к пицце, масло чесночное",
-            290, 67, restaurant1, restaurant1Menu0);
+            290, 67, restaurant1);
     public static final Dish dish8 = new Dish(DISH_ID_8, "Драники с жареной грудинкой и беконом",
             "подаются с жареной грудинкой, беконом и луком, сметаной, маринованными огурцами и зеленью",
-            290, 103, restaurant1, restaurant1Menu1);
+            290, 103, restaurant1);
     public static final Dish dish9 = new Dish(DISH_ID_9, "Салат с курицей и помидорами",
             "листья салата, морковь, куриное филе, помидоры, огурцы, заправка для салата",
-            290, 105, restaurant1, restaurant1Menu1);
+            290, 105, restaurant1);
 
     public static final Dish dish10 = new Dish(DISH_ID_10, "Тальята из говядины с вялеными томатами",
             "овядина на подушке из зелени",
-            340, 133, restaurant2, restaurant1Menu1);
+            340, 133, restaurant2);
     public static final Dish dish11 = new Dish(DISH_ID_11, "Тартар из говядины",
             "кубики говядины, сырой перепелиный желток, красный лук, каперсы, маринованный огурец",
-            250, 150, restaurant2, restaurant1Menu1);
+            250, 150, restaurant2);
     public static final Dish dish12 = new Dish(DISH_ID_12, "Суп кюфта-бозбаш",
             "ароматное сытное блюдо с тефтелей из баранины, нутом, рисом, картофелем и пряными травами",
-            150, 100, restaurant2, restaurant1Menu1);
+            150, 100, restaurant2);
 
     public static final Dish dish13 = new Dish(DISH_ID_13, "Еда не в меню",
             "без описания",
             290, 105, restaurant1);
+
+    static {
+        dish0.setMenus(Set.of(restaurant0Menu0, restaurant0Menu1));
+        dish1.setMenus(Set.of(restaurant0Menu0));
+        dish2.setMenus(Set.of(restaurant0Menu1));
+        dish3.setMenus(Set.of(restaurant0Menu1));
+        dish4.setMenus(Set.of(restaurant0Menu1));
+        dish5.setMenus(Set.of(restaurant1Menu0, restaurant1Menu1));
+        dish6.setMenus(Set.of(restaurant1Menu0));
+        dish7.setMenus(Set.of(restaurant1Menu0));
+        dish8.setMenus(Set.of(restaurant1Menu1));
+        dish9.setMenus(Set.of(restaurant1Menu1));
+        dish10.setMenus(Set.of(restaurant1Menu1));
+        dish11.setMenus(Set.of(restaurant1Menu1));
+        dish12.setMenus(Set.of(restaurant1Menu1));
+    }
 }
