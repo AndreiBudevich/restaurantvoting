be340402c2185af0700e8968da220c6388a48b6c
Andrei Budevich <budevichac01@gmail.com>
2022-04-22 16:55:43 +0300

    add utility class ValidationUtil

---
+++ src/main/java/by/restaurantvoting/util/validation/ValidationUtil.java
@@ -0,0 +1,29 @@
+package by.restaurantvoting.util.validation;
+
+import by.restaurantvoting.HasId;
+import by.restaurantvoting.error.IllegalRequestDataException;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ValidationUtil {
+
+    public static void checkNew(HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(HasId bean, int id) {
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.id() != id) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must has id=" + id);
+        }
+    }
+
+    public static void checkModification(int count, int id) {
+        if (count == 0) {
+            throw new IllegalRequestDataException("Entity with id=" + id + " not found");
+        }
+    }
+}
