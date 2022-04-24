9d67b23fff264f9cfe759a5f5edc3ec814ce2400
Andrei Budevich <budevichac01@gmail.com>
2022-04-24 11:33:56 +0300

    add MenuRepository

---
+++ src/main/java/by/restaurantvoting/repository/MenuRepository.java
@@ -0,0 +1,20 @@
+package by.restaurantvoting.repository;
+
+import by.restaurantvoting.model.Menu;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface MenuRepository extends BaseRepository<Menu> {
+
+    @Query("SELECT m FROM Menu m WHERE m.restaurant.id=?1 ORDER BY m.date DESC")
+    List<Menu> getAllMenusByRestaurantsId(int restaurantId);
+
+    @EntityGraph(attributePaths = {"dishes"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT m FROM Menu m WHERE m.id=?1")
+    Menu getWithDishes(int id);
+}
+
