c496fa695ff6ae0e45e4ee01b01e7b180f44be76
Andrei Budevich <budevichac01@gmail.com>
2022-04-24 11:34:49 +0300

    add VoteRepository

---
+++ src/main/java/by/restaurantvoting/repository/VoteRepository.java
@@ -0,0 +1,18 @@
+package by.restaurantvoting.repository;
+
+import by.restaurantvoting.model.Vote;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface VoteRepository extends BaseRepository<Vote> {
+
+    @Query("SELECT v FROM Vote v WHERE v.user.id = ?1 and v.date = ?2")
+    Vote getByDateUserId(int userId, LocalDate date);
+
+    @Query("SELECT v FROM Vote v  JOIN FETCH v.restaurant WHERE v.user.id = ?1")
+    List<Vote> getAllUserId(int userId);
+}
