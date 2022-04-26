0dde352c2b1a8ef30f1243469a0a9feae5e2b232
Andrei Budevich <budevichac01@gmail.com>
2022-04-26 13:47:57 +0300

    add UserVoteRestController

---
+++ src/main/java/by/restaurantvoting/web/vote/UserVoteRestController.java
@@ -0,0 +1,31 @@
+package by.restaurantvoting.web.vote;
+
+import by.restaurantvoting.model.Vote;
+import by.restaurantvoting.repository.VoteRepository;
+import by.restaurantvoting.web.SecurityUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping(value = UserVoteRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserVoteRestController {
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    protected static final String REST_URL = "api/rest/user/votes";
+
+    @GetMapping
+    public List<Vote> getAllByUserId() {
+        int userId = SecurityUtil.authId();
+        log.info("getAll votes for user {}", userId);
+        return voteRepository.getAllByUserId(userId);
+    }
+}
