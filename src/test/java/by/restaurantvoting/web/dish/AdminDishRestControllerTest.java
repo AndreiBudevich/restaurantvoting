56e80bcc3b86a382e8c0933e052fb629cd823aa8
Andrei Budevich <budevichac01@gmail.com>
2022-04-26 21:18:31 +0300

    add AdminDishRestControllerTest

---
+++ src/test/java/by/restaurantvoting/web/dish/AdminDishRestControllerTest.java
@@ -0,0 +1,115 @@
+package by.restaurantvoting.web.dish;
+
+import by.restaurantvoting.AbstractControllerTest;
+import by.restaurantvoting.model.Dish;
+import by.restaurantvoting.repository.DishRepository;
+import by.restaurantvoting.testdata.DishTestData;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static by.restaurantvoting.testdata.DishTestData.*;
+import static by.restaurantvoting.testdata.UserTestDate.ADMIN_MAIL;
+import static by.restaurantvoting.testdata.UserTestDate.USER0_MAIL;
+import static by.restaurantvoting.util.JsonUtil.writeValue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AdminDishRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminDishRestController.REST_URL.replace("{restaurantId}", "1") + '/';
+
+    @Autowired
+    DishRepository dishRepository;
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + DISH_ID_0))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(dish0));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + NOT_FOUND))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getUnauth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + DISH_ID_0))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER0_MAIL)
+    void getForbidden() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + DISH_ID_0))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(dish4, dish13, dish2, dish0, dish3, dish1));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + DISH_ID_13))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertFalse(dishRepository.findById(DISH_ID_13).isPresent());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void deleteNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + NOT_FOUND))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void update() throws Exception {
+        Dish updated = DishTestData.getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + DISH_ID_0)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        DISH_MATCHER.assertMatch(dishRepository.getById(DISH_ID_0), DishTestData.getUpdated());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void createWithLocation() throws Exception {
+        Dish newDish = DishTestData.getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(newDish)))
+                .andExpect(status().isCreated());
+        Dish created = DISH_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newDish.setId(newId);
+        DISH_MATCHER.assertMatch(created, newDish);
+        DISH_MATCHER.assertMatch(dishRepository.getById(newId), newDish);
+    }
+}
+
