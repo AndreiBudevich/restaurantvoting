c53e68f2b69e37b5275752a35437cba773caa0be
Andrei Budevich <budevichac01@gmail.com>
2022-04-29 18:07:33 +0300

    getWithVote->getAllWithVote, delete sort #getAllWithVote

--- src/main/java/by/restaurantvoting/repository/RestaurantRepository.java
+++ src/main/java/by/restaurantvoting/repository/RestaurantRepository.java
@@ -12,8 +12,8 @@ import java.util.List;
 public interface RestaurantRepository extends BaseRepository<Restaurant> {
 
     @EntityGraph(attributePaths = {"votes"}, type = EntityGraph.EntityGraphType.LOAD)
-    @Query("SELECT DISTINCT r FROM Restaurant r LEFT JOIN r.votes v WHERE v.date=?1 or v.restaurant IS NULL ORDER BY r.name ASC")
-    List<Restaurant> getWithVote(LocalDate date);
+    @Query("SELECT DISTINCT r FROM Restaurant r LEFT JOIN r.votes v WHERE v.date=?1 or v.restaurant IS NULL")
+    List<Restaurant> getAllWithVote(LocalDate date);
 
     @Query("SELECT r FROM Restaurant r JOIN r.menus m WHERE m.date=?1 ORDER BY r.name ASC")
     List<Restaurant> getAllWithMenu(LocalDate date);
