cc0e9b15a4882a2594a4cf27c30b516dba1a76f5
Andrei Budevich <budevichac01@gmail.com>
2022-04-30 12:48:23 +0300

    add validation groups

---
+++ src/main/java/by/restaurantvoting/View.java
@@ -0,0 +1,11 @@
+package by.restaurantvoting;
+
+import javax.validation.groups.Default;
+
+public class View {
+    public interface Web extends Default {
+    }
+
+    public interface Persist extends Default {
+    }
+}
