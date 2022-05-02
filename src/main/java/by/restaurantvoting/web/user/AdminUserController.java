712ecbecb7db4c94182ceed80766886dbe8ddda8
Andrei Budevich <budevichac01@gmail.com>
2022-05-02 19:25:40 +0300

    add description swagger AdminUserController

--- src/main/java/by/restaurantvoting/web/user/AdminUserController.java
+++ src/main/java/by/restaurantvoting/web/user/AdminUserController.java
@@ -1,6 +1,8 @@
 package by.restaurantvoting.web.user;
 
 import by.restaurantvoting.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
@@ -24,12 +26,14 @@ import static by.restaurantvoting.util.validation.ValidationUtil.checkNew;
 @RequestMapping(value = AdminUserController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 @CacheConfig(cacheNames = "users")
+@Tag(name = "Rest admin controller by user", description = "Allows the administrator to manage the users")
 public class AdminUserController extends AbstractUserController {
 
     static final String REST_URL = "/api/admin/users";
 
     @Override
     @GetMapping("/{id}")
+    @Operation(summary = "get user", description = "Allows you to get a user by its id")
     public ResponseEntity<User> get(@PathVariable int id) {
         return super.get(id);
     }
@@ -37,12 +41,14 @@ public class AdminUserController extends AbstractUserController {
     @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "delete user", description = "Allows you to delete a user by its id")
     public void delete(@PathVariable int id) {
         super.delete(id);
     }
 
     @GetMapping
     @Cacheable
+    @Operation(summary = "get all users", description = "Allows you to get all a users")
     public List<User> getAll() {
         log.info("getAll");
         return repository.findAll(Sort.by(Sort.Direction.ASC, "name", "email"));
@@ -50,6 +56,7 @@ public class AdminUserController extends AbstractUserController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @CacheEvict(allEntries = true)
+    @Operation(summary = "create user", description = "Allows you to create a user")
     public ResponseEntity<User> createWithLocation(@Valid @RequestBody User user) {
         log.info("create {}", user);
         checkNew(user);
@@ -63,6 +70,7 @@ public class AdminUserController extends AbstractUserController {
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @CacheEvict(allEntries = true)
+    @Operation(summary = "update user", description = "Allows you to update a user by its id")
     public void update(@Valid @RequestBody User user, @PathVariable int id) {
         log.info("update {} with id={}", user, id);
         assureIdConsistent(user, id);
@@ -70,6 +78,7 @@ public class AdminUserController extends AbstractUserController {
     }
 
     @GetMapping("/by-email")
+    @Operation(summary = "get user by email", description = "Allows you to get a user by its email")
     public ResponseEntity<User> getByEmail(@RequestParam String email) {
         log.info("getByEmail {}", email);
         return ResponseEntity.of(repository.getByEmail(email));
@@ -79,6 +88,7 @@ public class AdminUserController extends AbstractUserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
     @CacheEvict(allEntries = true)
+    @Operation(summary = "change the enable", description = "Allows you to change the enable property to active/inactive")
     public void enable(@PathVariable int id, @RequestParam boolean enabled) {
         log.info(enabled ? "enable {}" : "disable {}", id);
         User user = repository.getById(id);
